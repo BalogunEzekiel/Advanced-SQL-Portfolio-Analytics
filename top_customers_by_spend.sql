c63b6465d84544dd2f5657c9d25084bcfe9ca3f3
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-05-05 17:36:46 +0100

    Delete top_customers_by_spend.sql

--- top_customers_by_spend.sql
+++
@@ -1,9 +0,0 @@
-SELECT c.Customer_ID, c.Name, c.Province, c.Income, total_amount
-FROM customer c
-JOIN (SELECT Customer_ID, SUM(Amount) AS total_amount
-FROM transaction
-GROUP BY Customer_ID) t ON c.Customer_ID = t.Customer_ID
-WHERE t.total_amount = (SELECT MAX(total_sum)
-FROM (SELECT Customer_ID, SUM(Amount) AS total_sum
-FROM transaction
-GROUP BY Customer_ID) sub);
